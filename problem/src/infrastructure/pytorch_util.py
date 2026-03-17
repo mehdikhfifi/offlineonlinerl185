03434dbd1bb9c01d330b0fa5c33f400b7fcd2ec6
MehdiK <mehdikhfifi007@berkeley.edu>
2026-03-17 14:59:18 -0700

    done with layer norm

--- problem/src/infrastructure/pytorch_util.py
+++ problem/src/infrastructure/pytorch_util.py
@@ -54,7 +54,8 @@ def build_mlp(
     in_size = input_size
     for _ in range(n_layers):
         layers.append(nn.Linear(in_size, size))
-        # TODO(student): Add layer norm
+        # DONE (student): Add layer norm
+        layers.append(nn.LayerNorm(size))
         layers.append(activation)
         in_size = size
     layers.append(nn.Linear(in_size, output_size))
@@ -103,6 +104,7 @@ def build_ensemble_mlp(
         for _ in range(n_layers):
             layers.append(nn.Linear(in_size, size))
             # TODO(student): Add layer norm
+            layers.append(nn.LayerNrom(size))
             layers.append(activation.__class__())   # fresh module per layer
             in_size = size
         layers.append(nn.Linear(in_size, output_size))
